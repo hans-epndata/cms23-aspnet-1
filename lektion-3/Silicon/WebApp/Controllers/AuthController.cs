82b4282148729b1aae662253e9f7f6470d803e98
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2024-03-11 12:18:29 +0100

    lektion 3 - partials & formulär

---
+++ lektion-3/Silicon/WebApp/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+
+    #region SignIn
+    [Route("/login")]
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+    #endregion
+
+
+    #region SignUp
+    [Route("/register")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+    #endregion
+
+}
